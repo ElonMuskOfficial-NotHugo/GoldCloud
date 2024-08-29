117168965a54514d2847c697f76c929619715ae1
Liam Strickland <liam.strickland96@gmail.com>
2024-08-29 13:33:48 +0200

    update navbar style and layout, index header, add link to cart in notice, add account route and view, add product delete action from show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :redirect_logged_in_user, only: [:home]
+  before_action :authenticate_user!  # Ensures the user is logged in
 
   def home
     redirect_to products_path if user_signed_in?
@@ -7,6 +8,10 @@ class PagesController < ApplicationController
 
   def cart
     @order = current_order
+  end
+
+  def account
+    @user = current_user
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   resources :order_items, only: [:update]
 
   get 'cart', to: 'pages#cart'
+  get 'account', to: 'pages#account', as: 'account'
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
