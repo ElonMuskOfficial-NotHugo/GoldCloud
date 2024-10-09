05aea5b59bdf23475e094e527889cd455d15888c
Liam Strickland <liam.strickland96@gmail.com>
2024-10-09 15:13:24 +0200

    Add ability for admin to add a new package. Change admin nav to include dropup menu for adding products and packages. Update ruby version to 3.3.5. Move cart and checkout actions and routes from pages controller to orders controller for better separation of concerns

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,10 +1,23 @@
 class PackagesController < ApplicationController
   def new
-
+    @package = Package.new
   end
 
   def create
-
+    @package = Package.new(package_params)
+    @package.available = true
+    if @package.save
+      if params[:package][:product_ids].present?
+        params[:package][:product_ids].reject(&:blank?).each do |product_id|
+          PackageProduct.create(package: @package, product_id: product_id)
+        end
+      end
+      redirect_to item_path(@package.item), notice: 'Package was successfully created.'
+    else
+      Rails.logger.error("Package save failed: #{@package.errors.full_messages}")
+      flash.now[:alert] = "Failed to create package: #{@package.errors.full_messages.join(', ')}"
+      render :new
+    end
   end
 
   def update
@@ -14,6 +27,6 @@ class PackagesController < ApplicationController
   private
 
   def package_params
-    params.require(:package).permit(:name, :price, :available, photos: [])
+    params.require(:package).permit(:name, :description, :price, photos: [], product_ids: [])
   end
 end
