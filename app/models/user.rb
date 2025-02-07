1d15e6d159de542201e253afaf55a9c219ae8b2e
Liam Strickland <liam.strickland96@gmail.com>
2025-02-07 19:11:33 +0200

    add customer and admin order view, improve devise, other fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
 
   has_many :orders
   has_many :chats
@@ -15,5 +15,4 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :role, presence: true
   validates :username, presence: true, uniqueness: true
-  validates :address, presence: true
 end
